7670de2d6493d4588ea296e6a1cd57de8d7c1d2d
Alex <tanko77@rambler.ru>
2022-09-04 18:19:22 +0300

    2урок задача13

--- Program.cs
+++ Program.cs
@@ -3,6 +3,24 @@
 // 782 -> 8
 // 918 -> 1
 
-Console.Write("Введите трехзначное число: ");
+// Console.Clear();
+// Console.Write("Введите трехзначное число: ");
+// string input = Console.ReadLine();
+// Console.WriteLine("{0} -> {1}",input, input[1]);
+
+
+// Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+// 645 -> 5
+// 78 -> третьей цифры нет
+// 32679 -> 6
+
+Console.Clear();
+Console.Write("Введите число: ");
 string input = Console.ReadLine();
-Console.WriteLine("{0} -> {1}",input, input[1]);
+if (input.Length > 2)
+{
+  Console.WriteLine("Третья цифра: " + input[2]);
+}
+else {
+  Console.WriteLine("Третьей цифры нет");
+}
